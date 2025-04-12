ed4ae06e17640531513fdce5a856313afb36ed71
cnage <52753181+cnage@users.noreply.github.com>
2025-04-12 13:55:27 +0800

    Update and rename WenZhenBaZiProCrack.js to WenZhenBaZiPro.js

--- WenZhenBaZiPro.js
+++ WenZhenBaZiPro.js
@@ -1,5 +1,3 @@
-/***********************************
-
 > 应用名称：问真八字
 > 软件版本：1.2.2
 > 脚本作者：ddgksf2013
@@ -8,24 +6,15 @@
 > 更新时间：2023-08-04
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
-> 问题反馈：ddgksf2013@163.com
-> 特别提醒：如需转载请注明出处，谢谢合作！
-> 特别说明：⚠️⚠️⚠️
-          本脚本仅供学习交流使用，禁止转载售卖
-          ⚠️⚠️⚠️
 
 
 [rewrite_local]
-  
 # > 问真八字☆解锁钻石会员权限（2023-08-04）@ddgksf2013
-^https?:\/\/bzpp*\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://github.com/cnage/MoYu/raw/master/WenZhenBaZiProCrack.js
-
+^https?:\/\/bzpp.*\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://github.com/cnage/MoYu/raw/master/WenZhenBaZiPro.js
 
 [mitm] 
-
 hostname=bzpp*.iwzbz.com
 
-***********************************/
 
 
 
